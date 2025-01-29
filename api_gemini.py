1cfcaabce6fca83e49b792cbdbc3959ad944638e
Edson Copque <ecop@disroot.org>
2025-01-29 12:50:55 -0300

    Update api_gemini.py

--- api_gemini.py
+++ api_gemini.py
@@ -1,22 +1,28 @@
 # File: /Python/api_gemini.py
 
+# Install the Required Library:
 # pip install -U google-generativeai
 
+# Import the google.generativeai Library
 import google.generativeai as genai
 
 # Generate your API:
 # https://aistudio.google.com/app/apikey
 
+# Configure the API Key:
 API_KEY = 'AIzaSyAVMXBtSPA_HagSAU1XZR67wmEwbH1biMI'
 genai.configure(api_key=API_KEY)
 
+# List Available Models:
 for i1 in genai.list_models():
     if 'generateCotent' in i1.supported_generation_methods:
         print(i1.name)
 
+# Create a Content Generation Model:
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
 
+# Display Welcome Message to the User:
 welcome = '### Welcome to the eCop Assistant with Gemine AI ###'
 print(len(welcome) * '#')
 print(welcome)
@@ -24,6 +30,7 @@ print(len(welcome) * '#')
 print('### Enter "exit" to exit ###')
 print('')
 
+# User Interaction Loop:
 while True:
     text = input('Write your message: ')
     if text == 'exit':
@@ -32,4 +39,5 @@ while True:
     response = chat.send_message(text)
     print('Gemini:', response.text, '\n')
 
+# End of Chat Message:
 print('Chat ended.')
